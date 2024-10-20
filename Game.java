a8e3b0112ae60ce6058ad5eae6cd2dfeabe5300c
CamBD <164558064+CamBD@users.noreply.github.com>
2024-10-19 20:00:18 -0400

    Update Game.java
    Updated tile values and quantities to reflect scrabble rules.

--- Game.java
+++ Game.java
@@ -13,27 +13,25 @@ public class Game {
         this.tileStack.addTile('D',2,4);
         this.tileStack.addTile('E',1,12);
         this.tileStack.addTile('F',4,2);
-        this.tileStack.addTile('G',4,3);
+        this.tileStack.addTile('G',2,3);
         this.tileStack.addTile('H',4,2);
         this.tileStack.addTile('I',1,9);
         this.tileStack.addTile('J',8,1);
-        this.tileStack.addTile('K',5,12);
-        this.tileStack.addTile('L',1,2);
-        this.tileStack.addTile('M',3,9);
-        this.tileStack.addTile('N',1,2);
-        this.tileStack.addTile('O',1,2);
-        this.tileStack.addTile('P',3,4);
-        this.tileStack.addTile('Q',10,12);
-        this.tileStack.addTile('R',1,2);
-        this.tileStack.addTile('S',1,9);
-        this.tileStack.addTile('T',1,2);
-        this.tileStack.addTile('U',1,2);
-        this.tileStack.addTile('V',2,4);
-        this.tileStack.addTile('W',4,12);
-        this.tileStack.addTile('X',8,2);
-        this.tileStack.addTile('Y',4,12);
-        this.tileStack.addTile('Z',10,2);
-
-
+        this.tileStack.addTile('K',5,1);
+        this.tileStack.addTile('L',1,4);
+        this.tileStack.addTile('M',3,2);
+        this.tileStack.addTile('N',1,6);
+        this.tileStack.addTile('O',1,8);
+        this.tileStack.addTile('P',3,2);
+        this.tileStack.addTile('Q',10,1);
+        this.tileStack.addTile('R',1,6);
+        this.tileStack.addTile('S',1,4);
+        this.tileStack.addTile('T',1,6);
+        this.tileStack.addTile('U',1,4);
+        this.tileStack.addTile('V',4,2);
+        this.tileStack.addTile('W',4,2);
+        this.tileStack.addTile('X',8,1);
+        this.tileStack.addTile('Y',4,2);
+        this.tileStack.addTile('Z',10,1);
     }
 }
