c046cfefddc85f8df5049a72e8caecba0c7eebdb
JalterCode <jakester502@gmail.com>
2024-10-19 21:34:10 -0400

    implemented structure for board

--- Board.java
+++ Board.java
@@ -1,2 +1,26 @@
 public class Board {
+    private Tile[][] board = new Tile[15][15];
+
+    public Board(){
+        initializeBoard();
+        displayBoard();
+    }
+
+    public void displayBoard(){
+        for (int row = 0; row < 15; row++) {
+            for (int col = 0; col < 15; col++) {
+                System.out.print("  |  " + board[row][col].getLetter() + " ");
+            }
+            System.out.println("|");
+        }
+    }
+
+    public void initializeBoard(){
+        for (int row = 0; row < 15; row++) {
+            for (int col = 0; col < 15; col++) {
+                board[row][col] = new Tile('A', 0);
+            }
+        }
+    }
 }
+
