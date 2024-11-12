82b299a0793d49a50af1b1fc9e5cf8d781c90096
JalterCode <jakester502@gmail.com>
2024-11-12 17:45:00 -0500

    added player hand

--- View.java
+++ View.java
@@ -3,44 +3,47 @@ import javax.swing.*;
 import java.awt.event.*;
 import java.util.Locale;
  class View {
-    private JButton[][] buttons;
-    private final Game model;
+     private JButton[][] buttons;
+     private JPanel handPanel;
+     private Game model;
 
-    public View(Game model) {
-        this.model = model;
+     public View(Game model) {
+         this.model = model;
 
-        //initialize buttons
-        buttons = new JButton[15][15];
-        for (int row = 0; row < 15; row++) {
-            for (int col = 0; col < 15; col++) {
-                buttons[row][col] = new JButton();
-                buttons[row][col].setPreferredSize(new Dimension(25,25));
-            }
-        }
+         buttons = new JButton[15][15];
+         for (int row = 0; row < 15; row++) {
+             for (int col = 0; col < 15; col++) {
+                 buttons[row][col] = new JButton();
+                 buttons[row][col].setPreferredSize(new
+                         Dimension(25, 25));
+             }
+         }
 
-        JFrame frame = new JFrame();
-        JPanel container = new JPanel(new GridLayout(15, 15, 0, 0));
-        //initialize panels
-        for (int row = 0; row < 15; row++) {
-            for (int col = 0; col < 15; col++) {
-                new JPanel(new FlowLayout(FlowLayout.LEFT,0,0));
-                container.add(buttons[row][col]);
-            }
+         handPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+         handPanel.setPreferredSize(new Dimension(600, 50));
+         updateHandPanel();
 
-        }
+         JFrame frame = new JFrame();
+         JPanel container = new JPanel(new GridLayout(15, 15, 0, 0));
+         for (int row = 0; row < 15; row++) {
+             for (int col = 0; col < 15; col++) {
+                 container.add(buttons[row][col]);
+             }
+         }
 
-        //initialize guess panel
-        JPanel guessPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
-        guessPanel.setPreferredSize(new Dimension(600,150));
-        JButton answer = new JButton("Answer");
-        guessPanel.add(answer);
-        frame.add(guessPanel, BorderLayout.SOUTH);
+         frame.add(handPanel, BorderLayout.SOUTH);
+         frame.add(container, BorderLayout.NORTH);
+         frame.pack();
+         frame.setVisible(true);
+     }
 
-        frame.add(container, BorderLayout.NORTH);
-        frame.pack();
-        frame.setVisible(true);
-
-
-    }
-
-}
+     public void updateHandPanel() {
+         handPanel.removeAll();
+         for (Tile tile : model.getCurrentPlayer().getHand()) {
+             JButton tileButton = new JButton(String.valueOf(tile.getLetter()));
+             handPanel.add(tileButton);
+         }
+         handPanel.revalidate();
+         handPanel.repaint();
+     }
+ }
